0d0d3779b5126e42f420953fd7f0050727878179
Jeffrey Sabarese <ajaxstardust@duck.com>
2026-02-21 12:17:23 -0500

    deleted

--- public/tailwindcss.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace P2u2;
-error_reporting(0);
-define('NS2', __NAMESPACE__);
-define('NS2_ROOT', dirname(__DIR__));
-require NS2_ROOT . '/vendor/autoload.php';
-require  NS2_ROOT . '/src/View/Tailwindcss.page.php';
